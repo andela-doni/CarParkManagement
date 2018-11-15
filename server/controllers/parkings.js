5574fe35a85107ed6c1c0fe95edd7efb5c42f4c8
Deborah Oni <deborah.oni@andela.com>
2018-11-15 19:37:22 +0100

    create parking controller

--- server/controllers/parkings.js
+++ server/controllers/parkings.js
@@ -1,15 +1,19 @@
 const Parking = require('../../models').Parking;
+const Vehicle = require('../../models').Vehicle;
+const ParkingSlot = require('../../models').ParkingSlot;
 module.exports = {
   create(req, res) {
     return Parking
     .findOne({
       where: {
-        vehicleId: req.params.vehicleId,
-        parkingSlotId: req.params.parkingSlotId
+       $or: [
+         {vehicleId: req.body.vehicleId},
+          {parkingSlotId: req.body.parkingSlotId}          
+       
+       ]
       }
     })
     .then(parking => {
-      console.log('parking', parking)
       if(parking){
         return res.status(409).send({
           message: 'Parking record already exists, try another slot '
@@ -18,13 +22,64 @@ module.exports = {
       else{
       Parking
       .create({
-        parkingSlotId: req.params.parkingSlotId,
-        vehicleId: req.params.vehicleId,
+        parkingSlotId: req.body.parkingSlotId,
+        vehicleId: req.body.vehicleId,
       })
       .then(parking => res.status(201).send(parking))
 
       }
     })
      .catch(error => res.status(400).send(error));
-  }, 
+  },
+
+list(req, res) {
+  return Parking
+    .all()
+    .then(parking => res.status(200).send(parking))
+    .catch(error => res.status(400).send(error));
+},
+
+retrieve(req, res) {
+  return Parking
+    .findById(req.params.parkingId, {
+      include: [{
+        model: Vehicle,
+      }],
+    })
+    .then(parking => {
+      if (!parking) {
+        return res.status(404).send({
+          message: 'Parking Record Not Found',
+        });
+      }
+      return res.status(200).send(parking);
+    })
+    .catch(error => res.status(400).send(error));
+},
+
+  destroy(req, res) {
+  return Parking
+    .find({
+        where: {
+          parkingSlotId: req.body.parkingSlotId,
+          vehicleId: req.body.vehicleId,
+        },
+      })
+    .then(parking => {
+      if (!parking) {
+        return res.status(404).send({
+          message: 'Record not found',
+        });
+      }
+
+      return parking
+        .destroy()
+        .then(() => res.status(204).send({
+          message: 'Parking Record Removed',
+        }))
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+},
+
 };
